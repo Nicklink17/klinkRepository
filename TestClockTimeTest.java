3d3b841aa3ce790fee6c747ba59d5306e8a48309
klinkham <klink013@rangers.uwp.edu>
2023-04-26 20:45:37 -0500

    This is the branch repository

---
+++ TestClockTimeTest.java
@@ -0,0 +1,42 @@
+
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * The test class TestClockTimeTest.
+ *
+ * @author  (your name)
+ * @version (a version number or a date)
+ */
+public class TestClockTimeTest
+{
+    /**
+     * Default constructor for test class TestClockTimeTest
+     */
+    public TestClockTimeTest()
+    {
+    }
+
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    @BeforeEach
+    public void setUp()
+    {
+    }
+
+    /**
+     * Tears down the test fixture.
+     *
+     * Called after every test case method.
+     */
+    @AfterEach
+    public void tearDown()
+    {
+    }
+}
